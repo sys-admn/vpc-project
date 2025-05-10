368bd9fc8730dffeabc516dd4a421d4ec6a0ce14
Aly SALL <a.sall@groupeonepoint.com>
2025-05-10 21:40:14 +0200

    Simplify Terratest files to fix compilation errors

--- test/simple_test.go
+++ test/simple_test.go
@@ -1,13 +1,11 @@
 package test
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 // TestSimple is a simple test that always passes
-// This is useful for verifying that the test framework is working
 func TestSimple(t *testing.T) {
-	// This test always passes
 	assert.True(t, true, "True should be true")
 }
