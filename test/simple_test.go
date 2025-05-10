1f60e5c0075ce75baf9f5e3f045d51dd8b1a3575
Aly SALL <a.sall@groupeonepoint.com>
2025-05-10 21:03:47 +0200

    Add go.sum and simple test

---
+++ test/simple_test.go
@@ -0,0 +1,11 @@
+package test
+
+import (
+	"testing"
+)
+
+// TestSimple is a simple test that always passes
+// This is useful for verifying that the test framework is working
+func TestSimple(t *testing.T) {
+	// This test always passes
+}
