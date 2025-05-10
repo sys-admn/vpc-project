b87232c44e210d79e33a37c651027cc15117505b
Aly SALL <a.sall@groupeonepoint.com>
2025-05-10 21:22:51 +0200

    Simplify Go dependencies to fix checksum issues

--- test/simple_test.go
+++ test/simple_test.go
@@ -2,10 +2,12 @@ package test
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 // TestSimple is a simple test that always passes
 // This is useful for verifying that the test framework is working
 func TestSimple(t *testing.T) {
 	// This test always passes
+	assert.True(t, true, "True should be true")
 }
